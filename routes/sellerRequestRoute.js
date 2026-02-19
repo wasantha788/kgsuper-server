cd545bab36282b7b67c189429cb01fa4689903f8
wasantha788 <wasanthajayasinghe95@gmail.com>
2026-02-20 01:04:19 +0530

    Change SMTP port from 587 to 465
    fix

--- routes/sellerRequestRoute.js
+++ routes/sellerRequestRoute.js
@@ -45,7 +45,7 @@ const upload = multer({
 const createTransporter = () => {
   return nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
+    port: 465,
     secure: false,
     auth: {
       user: process.env.EMAIL_USER,
