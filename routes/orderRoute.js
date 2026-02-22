fb6f874bafeb7bfbd158abc3ea51ec31574f6196
Wasantha Jayasinghe <wasanthajayasinghe95@gmail.com>
2026-02-22 14:44:41 +0530

    pdf

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -20,7 +20,7 @@ const orderRouter = express.Router();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port:  465,
   secure: false, // Must be false for port 587
   auth: {
     user: process.env.EMAIL_USER,
